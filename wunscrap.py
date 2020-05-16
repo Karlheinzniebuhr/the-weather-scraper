43c5a13b99c030bb36fc8e54fb45a784c7a630b4
karlheinzniebuhr <karl.booklover@gmail.com>
2020-05-15 20:58:20 -0400

    added date to time row

--- wunscrap.py
+++ wunscrap.py
@@ -1,6 +1,7 @@
 # Made with love by Karl. Telegram: @karlpy
 
 import datetime
+from datetime import datetime as dt
 import requests
 from lxml import etree
 import lxml.html as lh
@@ -9,28 +10,37 @@ import unicodedata
 from pymongo import MongoClient
 
 # configuration
-stationsFile = open('stations.txt', 'r') 
-urls = stationsFile.readlines() 
-todaysDate = datetime.datetime.today()
-numberOfDays = 30
+stations_file = open('stations.txt', 'r') 
+urls = stations_file.readlines() 
+todays_date = datetime.datetime.today()
+number_of_days = 3
 dbclient = MongoClient('localhost:27017')
 wunderground_db = dbclient['wunderground']
 
-def formatKey(key):
+def format_key(key):
     return key.replace(' ','_').replace('.','')
 
-def scrapStation(weather_station_url):
-    url_generator = (f'{weather_station_url}/table/{(todaysDate - datetime.timedelta(days=x)).strftime("%Y-%m-%d")}/{(todaysDate - datetime.timedelta(days=x)).strftime("%Y-%m-%d")}/daily' for x in range(numberOfDays))
+def date_difference(day):
+    return (todays_date - datetime.timedelta(days=day)).strftime("%Y-%m-%d")
+
+def url_generator(weather_station_url, number_of_days):
+    for day in range(number_of_days):
+        date_string = date_difference(day)
+        url = f'{weather_station_url}/table/{date_string}/{date_string}/daily'
+        yield date_string, url
+
+def scrap_station(weather_station_url):
+
+    url_gen = url_generator(weather_station_url, number_of_days)
 
     parser = etree.HTMLParser(recover=True)
     session = requests.Session()
     collection_name = f'{weather_station_url}'
 
-    for url in url_generator:
+    for date_string, url in url_gen:
         print(f'get url: {url}')
         html_string = session.get(url)
         doc = lh.fromstring(html_string.content)
-        history_table = doc.xpath('//*[@id="inner-content"]/section[1]/div[1]/div/div/div/div/lib-history/div[2]/lib-history-table/div/div/div/table/tbody')
         history_table = doc.xpath('//*[@id="inner-content"]/section[1]/div[1]/div/div/div/div/lib-history/div[2]/lib-history-table/div/div/div/table/tbody')
         if not history_table:
             continue
@@ -45,7 +55,15 @@ def scrapStation(weather_station_url):
         for tr in tr_elements[1:]:
             row_dict = {}
             for i, td in enumerate(tr.getchildren()):
-                row_dict[formatKey(headers_list[i])] = unicodedata.normalize("NFKD", td.text_content())
+                td_content = unicodedata.normalize("NFKD", td.text_content())
+
+                # replace time with datetime in first column
+                if i == 0:
+                    td_content = f'{date_string} {td_content}'
+                    date_time = dt.strptime(td_content, "%Y-%m-%d %I:%M %p")
+                    row_dict[format_key(headers_list[i])] = td_content
+                else:
+                    row_dict[format_key(headers_list[i])] = td_content
             data_rows.append(row_dict)
 
         print(f'Saving {len(data_rows)} rows')
@@ -54,4 +72,4 @@ def scrapStation(weather_station_url):
 for url in urls:
     url = url.strip()
     print(url)
-    scrapStation(url)
+    scrap_station(url)
